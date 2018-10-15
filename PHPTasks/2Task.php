16cb97138648da1d92ce12b36fb4affffb5a1dc1
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-10-15 18:49:38 +0700

    2Task выполнен вроде бы

---
+++ PHPTasks/2Task.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.10.2018
+ * Time: 18:16
+ */
+error_reporting(E_ALL);
+//1--------------------------------------
+
+const FIRST_CONSTANT=42;
+
+//2-------------------------------------
+
+if(FIRST_CONSTANT){
+    echo "Существует переменная firstConstant</br>".PHP_EOL;
+}else{
+    echo "Не существует переменная firstConstant</br>".PHP_EOL;
+}
+
+//3---------------------------------------
+
+echo 'Значение моей константы равно = '.FIRST_CONSTANT.'.</br>'.PHP_EOL;
+
+//4---------------------------------------
+
+//FIRST_CONSTANT = 35;
+//Выдает ошибку Expression is not assignable: Constant reference
