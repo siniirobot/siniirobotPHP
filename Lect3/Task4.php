f888010d0ed21b06e750bcf76a941846581de17c
siniirobot <siniirobot@gmail.com>
2018-10-17 21:03:31 +0700

    lect3

---
+++ Lect3/Task4.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinii
+ * Date: 17.10.2018
+ * Time: 20:51
+ */
+
+
---
+++ Lect3/Test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinii
+ * Date: 17.10.2018
+ * Time: 19:44
+ */
