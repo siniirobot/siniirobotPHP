e9b408c20b5fd03fa080a520fa1de7ea5ccb7d20
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-10-27 21:24:08 +0700

    Lect3 Task1

--- Lect3/Task1.php
+++ Lect3/Task1.php
@@ -6,13 +6,36 @@
  * Time: 10:31
  */
 
+header('Content-Type: text/html; charset=utf-8');
+error_reporting(E_ALL);
+
+$number = isset($_GET['number']) ? intval($_GET['number']) : 0;
+
+if (isset($_GET['random'])) {
+    $random = intval($_GET['random']);
+    if ($number > $random) {
+        echo "Надо меньше";
+    } elseif ($number < $random) {
+        echo "Нужо больше";
+    }else{
+        echo "Это оно поздравляю";
+    }
+}else{
+    $random = rand(0, 100);
+}
 ?>
 
 <html>
 <head>
-
+    <title>Угадай число</title>
 </head>
 <body>
-
+<p>Попробуйте ввести число от 0 до 100 и угадать.</p>
+<form action="Task1.php" method="get">
+    <input type="text" name="number" value="<?=$number?>">
+    </br>
+    <input type="submit" value="Это число?">
+    <input type="hidden" name="random" value="<?=$random?>" >
+</form>
 </body>
 </html>
