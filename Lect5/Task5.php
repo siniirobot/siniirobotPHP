56fe7bffe8ab07731bf54fb5ba89e53ed688a655
siniirobotRab <siniirobot@gmail.com>
2018-10-31 15:31:20 +0600

    Lect5 Task5

--- Lect5/Task5.php
+++ Lect5/Task5.php
@@ -6,13 +6,39 @@
  * Time: 15:49
  */
 
-header('Content-Type: test/html; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 error_reporting(E_ALL);
 
 $fp = fopen('users.csv', 'rb');
+$i = 0;
+$j = 0;
 
-while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
-    echo $data[0].' '.$data[1].' '.$data[2].' '.$data[3].'</br>';
+if (isset($_POST['firstPage'])){
+    $page = 0;
+}else{
+    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
+}
+
+if (isset($_POST['back']) && $_POST['back'] && $page > 0){
+    $page -= 1;
+}else{
+    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
+}
+
+if (isset($_POST['page']) && $_POST['page']){
+    $page = $_POST['page'];
+}
+
+if (isset($_POST['forward']) && $_POST['forward'] && $page < 49){
+    $page += 1;
+}else{
+    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
+}
+
+if (isset($_POST['lastPage']) && $_POST['lastPage']){
+    $page = 49;
+}else{
+    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
 }
 
 ?>
@@ -23,6 +49,35 @@ while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
     <title>20 на странице</title>
 </head>
 <body>
+    <div>
+        <?php
+        if ($fp) {
+            while (($data = fgetcsv($fp, 20, ",")) !== FALSE) {
+                $i++;
+                if ($i / 20 > $page) {
+                    echo $i.'-'.$data[0] . ' ' . $data[1] . ' ' . $data[2] . ' ' . $data[3] . '</br>';
+                    $j++;
+                    if ($j == 20){
+                        $j = 0;
+                        break;
+                    }
+                }
+            }
+        } else {
+            echo 'Проблемы с открытием файла';
+        } ?>
+    </div>
+    <div>
+        <form action="Task5.php" method="post">
+            <input style="width: 150px" type="submit" name="firstPage" value="На первую страницу">
+            <input type="submit" name="back" value="&#8592;">
+            <input type="hidden" name="back" value="<?= $page -1 ?>">
+            <input type="text" name="page" value="<?= $page ?>" style="width: 30px">
+            <input type="submit" name="forward" value="&#8594;">
+            <input type="hidden" name="forward" value="<?= $page +1 ?>">
+            <input style="width: 150px" type="submit" name="lastPage" value="На последнюю страницу">
+        </form>
+    </div>
 
 </body>
 </html>
