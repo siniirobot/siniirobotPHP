67246b2ad80d644ec7662d75ae155d0d4ffc1573
siniirobotRab <siniirobot@gmail.com>
2018-11-01 16:20:06 +0600

    Lect5 Task5

--- Lect5/Task5.php
+++ Lect5/Task5.php
@@ -12,34 +12,20 @@ error_reporting(E_ALL);
 $fp = fopen('users.csv', 'rb');
 $i = 0;
 $j = 0;
+$page = isset($_POST['page']) ? intval($_POST['page']) : 0;
 
-if (isset($_POST['firstPage'])){
+if (isset($_POST['firstPage']) && $_POST['firstPage']) {
     $page = 0;
-}else{
-    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
-}
-
-if (isset($_POST['back']) && $_POST['back'] && $page > 0){
-    $page -= 1;
-}else{
-    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
-}
-
-if (isset($_POST['page']) && $_POST['page']){
-    $page = $_POST['page'];
-}
-
-if (isset($_POST['forward']) && $_POST['forward'] && $page < 49){
-    $page += 1;
-}else{
-    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
-}
-
-if (isset($_POST['lastPage']) && $_POST['lastPage']){
+} elseif (isset($_POST['back']) && $_POST['back'] && $page > 0) {
+    $page--;
+} elseif (isset($_POST['forward']) && $_POST['forward'] && $page < 49) {
+    $page++;
+} elseif (isset($_POST['lastPage']) && $_POST['lastPage']) {
     $page = 49;
-}else{
+} else {
     $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
 }
+
 
 ?>
 
@@ -49,35 +35,35 @@ if (isset($_POST['lastPage']) && $_POST['lastPage']){
     <title>20 на странице</title>
 </head>
 <body>
-    <div>
-        <?php
-        if ($fp) {
-            while (($data = fgetcsv($fp, 20, ",")) !== FALSE) {
-                $i++;
-                if ($i / 20 > $page) {
-                    echo $i.'-'.$data[0] . ' ' . $data[1] . ' ' . $data[2] . ' ' . $data[3] . '</br>';
-                    $j++;
-                    if ($j == 20){
-                        $j = 0;
-                        break;
-                    }
+<div>
+    <?php
+    if ($fp) {
+        while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
+            $i++;
+            if ($i / 20 > $page) {
+                echo $i . '-' . $data[0] . ' ' . $data[1] . ' ' . $data[2] . ' ' . $data[3] . '</br>';
+                $j++;
+                if ($j == 20) {
+                    $j = 0;
+                    break;
                 }
             }
-        } else {
-            echo 'Проблемы с открытием файла';
-        } ?>
-    </div>
-    <div>
-        <form action="Task5.php" method="post">
-            <input style="width: 150px" type="submit" name="firstPage" value="На первую страницу">
-            <input type="submit" name="back" value="&#8592;">
-            <input type="hidden" name="back" value="<?= $page -1 ?>">
-            <input type="text" name="page" value="<?= $page ?>" style="width: 30px">
-            <input type="submit" name="forward" value="&#8594;">
-            <input type="hidden" name="forward" value="<?= $page +1 ?>">
-            <input style="width: 150px" type="submit" name="lastPage" value="На последнюю страницу">
-        </form>
-    </div>
+        }
+    } else {
+        echo 'Проблемы с открытием файла';
+    } ?>
+</div>
+<div>
+    <form action="Task5.php" method="post">
+        <input style="width: 150px" type="submit" name="firstPage" value="На первую страницу">
+        <input type="submit" name="back" value="&#8592;">
+        <input type="hidden" name="back" value="<?= $page - 1 ?>">
+        <input type="text" name="page" value="<?= $page ?>" style="width: 30px">
+        <input type="submit" name="forward" value="&#8594;">
+        <input type="hidden" name="forward" value="<?= $page + 1 ?>">
+        <input style="width: 150px" type="submit" name="lastPage" value="На последнюю страницу">
+    </form>
+</div>
 
 </body>
 </html>
