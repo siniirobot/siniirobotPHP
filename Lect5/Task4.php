a9f7ebf48f33c2e2cd42265716980d3ef5e57821
siniirobotRab <siniirobot@gmail.com>
2018-11-01 13:54:57 +0600

    Lect5 Task4

--- Lect5/Task4.php
+++ Lect5/Task4.php
@@ -20,6 +20,12 @@ $departments = array();
 $sumSalary = 0;
 $sumAges = 0;
 
+$sumSalaryByDepartments = 0;
+$sumAgesByDepartments = 0;
+$avgSalaryByDepartments = 0;
+$avgAgeByDepartments = 0;
+$luckyPersonFromDep = [];
+$unluckyPersonFromDep = [];
 
 if ($fp) {
     $i = 0;
@@ -41,72 +47,78 @@ if ($fp) {
         } else {
             $unluckyPerson = $data;
         }
-        if (!(array_key_exists($data[1], $departments))) {
-            $fp2 = fopen('users.csv', 'rb');
-            $employees = 0;
-            $sumSalaryByDepartments = 0;
-            $sumAgesByDepartments = 0;
-            $avgSalaryByDepartments = 0;
-            $avgAgeByDepartments = 0;
-            $luckyPersonFromDep = [];
-            $unluckyPersonFromDep = [];
-            while (($data2 = fgetcsv($fp2, 1000, ",")) !== FALSE) {
-                if ($data[1] == $data2[1]) {
-                    $sumSalaryByDepartments += $data2[3];
-                    $sumAgesByDepartments += $data2[2];
-                    $employees++;
-                    if (count($luckyPersonFromDep) > 0) {
-                        if ($luckyPersonFromDep[3] < $data2[3]) {
-                            $luckyPersonFromDep = $data2;
-                        }
-                    } else {
-                        $luckyPersonFromDep = $data2;
-                    }
-                    if (count($unluckyPersonFromDep) > 0) {
-                        if ($unluckyPersonFromDep[3] > $data2[3]) {
-                            $unluckyPersonFromDep = $data2;
-                        }
-                    } else {
-                        $unluckyPersonFromDep = $data2;
-                    }
-                }
+        if (array_key_exists($data[1], $departments)) {
+            if (count($departments[$data[1]][0]) > 0) {
+                $departments[$data[1]][0]++;
+            } else {
+                $departments[$data[1]][0] = 1;
             }
-            $departments[$data[1]] = [
-                $employees,
-                $avgSalaryByDepartments = $sumSalaryByDepartments / $employees,
-                $sumAgesByDepartments = $sumAgesByDepartments / $employees,
-                $luckyPersonFromDep,
-                $unluckyPersonFromDep,
-            ];
-        }
-    }
-    fclose($fp);
-    if ($i > 0) {
-        $avgSalary = $sumSalary / $i;
-        echo 'Средняя ЗП: ' . number_format($avgSalary, 2, ',', ' ') . '</br>';
-        $avgAge = $sumAges / $i;
-        echo 'Средний возраст:' . $avgAge . '</br>';
-        echo 'Счастливчик: ' . $luckyPerson[0] . ' c ЗП: ' . number_format($luckyPerson[3], 2, ',', ' ') . '</br>';
-        echo 'Неудачник: ' . $unluckyPerson[0] . ' c ЗП: ' . number_format($unluckyPerson[3], 2, ',', ' ') . '</br>' . '</br>';
-        echo 'Статистика по отделам: ' . '</br>';
-        foreach ($departments as $key => $value) {
-            echo $key . ':</br>'
-                . 'Колличесвто сотрудников в отделе: ' . $value[0] . '</br>'
-                . 'Средняя зарплата по отделу: ' . number_format($value[1], 2, ',', ' ') . '</br>'
-                . 'Средний возраст по отделу: ' . intval($value[2]) . '</br>'
-                . 'Самый оплачиваемый сотрудник отдела:' . $value[3][0] . ' с ЗП: ' . number_format($value[3][3], 2, ',', ' ') . '</br>'
-                . 'Самы перспективный сотрудник отдела:' . $value[4][0] . ' с ЗП: ' . number_format($value[4][3], 2, ',', ' ') . '</br>';
-        }
-        echo 'Список самых оплачиваемых сотрудников отделов:' . '</br>';
-        foreach ($departments as $key => $value) {
-            echo $key . ': ' . $value[3][0] . ' с ЗП: ' . number_format($value[3][3], 2, ',', ' ') . '</br>';
-        }
-        echo 'Список самых перспективных сотрудников отделов:' . '</br>';
-        foreach ($departments as $key => $value) {
-            echo $key . ': ' . $value[4][0] . ' с ЗП: ' . number_format($value[4][3], 2, ',', ' ') . '</br>';
-        }
-    } else {
-        echo 'No Data';
-    }
 
+            if (count($departments[$data[1]][1]) > 0) {
+                $departments[$data[1]][1] += $data[3];
+            } else {
+                $departments[$data[1]][1] = $data[3];
+            }
+
+            if (count($departments[$data[1]][2]) > 0) {
+                $departments[$data[1]][2] += $data[2];
+            } else {
+                $departments[$data[1]][2] = $data[2];
+            }
+
+            if (count($departments[$data[1]][3]) > 0) {
+                if ($departments[$data[1]][3][3] < $data[3])
+                    $departments[$data[1]][3] = $data;
+            } else {
+                $departments[$data[1]][3] = $data;
+            }
+
+            if (count($departments[$data[1]][4]) > 0) {
+                if ($departments[$data[1]][4][3] > $data[3])
+                    $departments[$data[1]][4] = $data;
+            } else {
+                $departments[$data[1]][4] = $data;
+            }
+
+
+        } else {
+            $departments[$data[1]] = [
+                null,
+                null,
+                null,
+                null,
+                null,
+            ];
+
+        }
+    }
+}
+fclose($fp);
+
+if ($i > 0) {
+    $avgSalary = $sumSalary / $i;
+    echo 'Средняя ЗП: ' . number_format($avgSalary, 2, ',', ' ') . '</br>';
+    $avgAge = $sumAges / $i;
+    echo 'Средний возраст:' . $avgAge . '</br>';
+    echo 'Счастливчик: ' . $luckyPerson[0] . ' c ЗП: ' . number_format($luckyPerson[3], 2, ',', ' ') . '</br>';
+    echo 'Неудачник: ' . $unluckyPerson[0] . ' c ЗП: ' . number_format($unluckyPerson[3], 2, ',', ' ') . '</br>' . '</br>';
+    echo 'Статистика по отделам: ' . '</br>';
+    foreach ($departments as $key => $value) {
+        echo $key . ':</br>'
+            . 'Колличесвто сотрудников в отделе: ' . $value[0] . '</br>'
+            . 'Средняя зарплата по отделу: ' . number_format($value[1] /= $value[0], 2, ',', ' ') . '</br>'
+            . 'Средний возраст по отделу: ' . intval($value[2] /= $value[0]) . '</br>'
+            . 'Самый оплачиваемый сотрудник отдела:' . $value[3][0] . ' с ЗП: ' . number_format($value[3][3], 2, ',', ' ') . '</br>'
+            . 'Самы перспективный сотрудник отдела:' . $value[4][0] . ' с ЗП: ' . number_format($value[4][3], 2, ',', ' ') . '</br></br>';
+    }
+    echo 'Список самых оплачиваемых сотрудников отделов:' . '</br>';
+    foreach ($departments as $key => $value) {
+        echo $key . ': ' . $value[3][0] . ' с ЗП: ' . number_format($value[3][3], 2, ',', ' ') . '</br>';
+    }
+    echo '</br>' . 'Список самых перспективных сотрудников отделов:' . '</br>';
+    foreach ($departments as $key => $value) {
+        echo $key . ': ' . $value[4][0] . ' с ЗП: ' . number_format($value[4][3], 2, ',', ' ') . '</br>';
+    }
+} else {
+    echo 'No Data';
 }
