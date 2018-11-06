353ce95df27305016a90fd141b1ff8354ee576e1
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2018-11-06 07:24:39 +0700

    Lect5 Task5

--- Lect5/Task5.php
+++ Lect5/Task5.php
@@ -12,21 +12,32 @@ error_reporting(E_ALL);
 $fp = fopen('users.csv', 'rb');
 $i = 0;
 $j = 0;
-$page = isset($_POST['page']) ? intval($_POST['page']) : 0;
-
+$start = 0;
 
 if (isset($_POST['firstPage']) && $_POST['firstPage']) {
-    $page = 0;
-} elseif (isset($_POST['back']) && $_POST['back'] && $page > 0) {
-    $page--;
-} elseif (isset($_POST['forward']) && $_POST['forward'] && $page < 49) {
-    $page++;
-} elseif (isset($_POST['lastPage']) && $_POST['lastPage']) {
-    $page = 49;
-} else {
-    $page = isset($_POST['page']) ? intval($_POST['page']) : 0;
-}
+    $start = 0;
+    echo 'Это страница 0';
+}else
 
+if (isset($_POST['back']) && $_POST['back'] && $start > 0) {
+    $start = $start - 1;
+    echo 'Это страница назад';
+}else
+
+if ($_POST['forward'] && $start < 49) {
+    $start = $start + 1;
+    echo 'Это страница вперед';
+}else
+
+if (isset($_POST['lastPage']) && $_POST['lastPage']) {
+    $start = 49;
+    echo 'Это страница 49';
+}else
+if (isset($_POST['page'])){
+    $start = $_POST['page'];
+    echo 'это страница'.$start;
+}
+echo '</br>'.'текущая страница'.$start;
 
 ?>
 
@@ -41,7 +52,7 @@ if (isset($_POST['firstPage']) && $_POST['firstPage']) {
     if ($fp) {
         while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
             $i++;
-            if ($i / 20 > $page) {
+            if ($i / 20 > $start) {
                 echo $i . '-' . $data[0] . ' ' . $data[1] . ' ' . $data[2] . ' ' . $data[3] . '</br>';
                 $j++;
                 if ($j == 20) {
@@ -58,13 +69,40 @@ if (isset($_POST['firstPage']) && $_POST['firstPage']) {
     <form action="Task5.php" method="post">
         <input style="width: 150px" type="submit" name="firstPage" value="На первую страницу">
         <input type="submit" name="back" value="&#8592;">
-        <input type="hidden" name="back" value="<?= $page - 1 ?>">
-        <input type="text" name="page" value="<?= $page ?>" style="width: 30px">
+
+        <input type="text" name="page" value="<?=$start?>" style="width: 30px">
         <input type="submit" name="forward" value="&#8594;">
-        <input type="hidden" name="forward" value="<?= $page + 1 ?>">
+
         <input style="width: 150px" type="submit" name="lastPage" value="На последнюю страницу">
     </form>
 </div>
 
 </body>
 </html>
+<?php
+if ($_POST['page']) {
+    echo 'Этак кнопка page правда'.'</br>';
+}else{
+    echo 'Этак кнопка page не правда'.'</br>';
+}
+if ($_POST['firstPage']) {
+    echo 'Этак кнопка firstPage правда'.'</br>';
+}else{
+    echo 'Этак кнопка firstPage не правда'.'</br>';
+}
+if ($_POST['back']) {
+    echo 'Этак кнопка back правда'.'</br>';
+}else{
+    echo 'Этак кнопка back не правда'.'</br>';
+}
+if ($_POST['forward']) {
+    echo 'Этак кнопка forward правда'.'</br>';
+}else{
+    echo 'Этак кнопка forward не правда'.'</br>';
+}
+if ($_POST['lastPage']) {
+    echo 'Этак кнопка lastPage правда'.'</br>';
+}else{
+    echo 'Этак кнопка lastPage не правда'.'</br>';
+}
+?>
