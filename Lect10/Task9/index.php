0a26fdb53bf56b664665e14b84549aa60bc0476b
siniirobotRab <siniirobot@gmail.com>
2018-12-11 16:02:19 +0700

    Lect10 Task9

--- Lect10/Task9/index.php
+++ Lect10/Task9/index.php
@@ -49,7 +49,8 @@ try {
         header('Location:index.php');
         exit();
     }
-    if (isset($_GET['edit'])){
+
+    if (isset($_GET['edit']) || isset($_POST['save'])) {
         $page = 2;
         $editRow = $pdo->prepare('SELECT * FROM animals WHERE id = ?');
         $editRow->execute([$_GET['edit']]);
@@ -57,18 +58,22 @@ try {
         $lastId = isset($_POST['id']) ? intval($_POST['id']) : $editRow['id'];
         $name = isset($_POST['name']) ? $_POST['name'] : $editRow['name'];
         $species = isset($_POST['species']) ? $_POST['species'] : $editRow['species'];
-        $weight = isset($_POST)$editRow['weight'];
-        $gender = $editRow['gender'];
-        $comments = $editRow['comments'];
-        if (isset($_POST['save'])){
+        $weight = isset($_POST['weight']) ? $_POST['weight'] : $editRow['weight'];
+        $gender = isset($_POST['gender']) ? $_POST['gender'] : $editRow['gender'];
+        $comments = isset($_POST['comments']) ? $_POST['comments'] : $editRow['comments'];
+        if (isset($_POST['save'])) {
+            var_dump($editId);
             $pdo->exec("UPDATE animals 
-SET id = $lastId
-AND name = $name
-AND species = $species
-AND weight = $weight
-AND gender = $gender
-AND comments = $comments".'
-WHERE id = $editRow['.'id'.']');
+SET 
+id = $lastId, 
+name =" . '\'' . $name . '\'' . ", 
+species =" . '\'' . $species . '\'' . ", 
+weight = $weight, 
+gender = " . '\'' . $gender . '\'' . " , 
+comments = " . '\'' . $comments . '\'' . " 
+WHERE id = $editId");
+            $page = 2;
+
         }
     }
 } catch (PDOException $e) {
@@ -81,82 +86,33 @@ WHERE id = $editRow['.'id'.']');
     <title>Задание 9</title>
 </head>
 <body>
-<?php if ($page == 1) {?>
-<form action="index.php" method="get">
-    <table border="1">
-        <thead>
-        <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>species</td>
-            <td>weight</td>
-            <td>gender</td>
-            <td>comments</td>
-            <td>Удаление</td>
-            <td>Редактирование</td>
-        </tr>
-        </thead>
-        <tbody>
-        <?php while ($row = $stmt->fetch()) {
-
-            echo '<tr>';
-            foreach ($row as $key) {
-                echo '<td>' . $key . '</td>';
-            }
-            echo '<td><form action="index.php" method="get"><a href="index.php?del= ' . $row['id'] . '">Удалить</a></form></td>';
-            echo '<td><form action="index.php" method="get"><a href="index.php?edit=' . $row['id'] . ' ">Редактировать</a></form></td>';
-        } ?>
-        <?php
-
-        ?>
-        </tbody>
-    </table>
-</form>
-<form action="index.php" method="post">
-    <table border="1">
-        <thead>
-        <tr>
-            <td>Название поля</td>
-            <td>Поле ввода</td>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Id</td>
-            <td><input type="text" name="id" placeholder="<?= $lastId ?>"></td>
-        </tr>
-        <tr>
-            <td>Имя животного</td>
-            <td><input type="text" name="name" placeholder="<?= 'Введите имя питомца.' ?>"
-                       pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
-        </tr>
-        <tr>
-            <td>Вид животного</td>
-            <td><input type="text" name="species" placeholder="<?= 'Введите вид питомца.' ?>"
-                       pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
-        </tr>
-        <tr>
-            <td>Вес животного</td>
-            <td><input type="text" name="weight" placeholder="<?= 'Введите вес питомца.' ?>" pattern="^[0-9]+$"
-                       required></td>
-        </tr>
-        <tr>
-            <td>Пол животного</td>
-            <td><input type="text" name="gender" placeholder="<?= 'Введите пол питомца в виде М или Ж.' ?>"
-                       pattern="[МмЖж]" required></td>
-        </tr>
-        <tr>
-            <td>Комментарии</td>
-            <td><input type="text" name="comments" placeholder="<?= 'Комментарии.' ?>"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" name="add" value="Добавить" style="width: 100%" "></td>
-        </tr>
-        </tbody>
-    </table>
-</form>
-<?}else{?>
+<?php if ($page == 1) { ?>
+    <form action="index.php" method="get">
+        <table border="1">
+            <thead>
+            <tr>
+                <td>id</td>
+                <td>name</td>
+                <td>species</td>
+                <td>weight</td>
+                <td>gender</td>
+                <td>comments</td>
+                <td>Удаление</td>
+                <td>Редактирование</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php while ($row = $stmt->fetch()) {
+                echo '<tr>';
+                foreach ($row as $key) {
+                    echo '<td>' . $key . '</td>';
+                }
+                echo '<td><form action="index.php" method="get"><a href="index.php?del= ' . $row['id'] . '">Удалить</a></form></td>';
+                echo '<td><form action="index.php" method="get"><a href="index.php?edit=' . $row['id'] . ' ">Редактировать</a></form></td>';
+            } ?>
+            </tbody>
+        </table>
+    </form>
     <form action="index.php" method="post">
         <table border="1">
             <thead>
@@ -172,27 +128,73 @@ WHERE id = $editRow['.'id'.']');
             </tr>
             <tr>
                 <td>Имя животного</td>
-                <td><input type="text" name="name" placeholder="<?=$name?>"
+                <td><input type="text" name="name" placeholder="<?= 'Введите имя питомца.' ?>"
                            pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
             </tr>
             <tr>
                 <td>Вид животного</td>
-                <td><input type="text" name="species" placeholder="<?=$species?>"
+                <td><input type="text" name="species" placeholder="<?= 'Введите вид питомца.' ?>"
                            pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
             </tr>
             <tr>
                 <td>Вес животного</td>
-                <td><input type="text" name="weight" placeholder="<?=$weight?>" pattern="^[0-9]+$"
+                <td><input type="text" name="weight" placeholder="<?= 'Введите вес питомца.' ?>" pattern="^[0-9]+$"
                            required></td>
             </tr>
             <tr>
                 <td>Пол животного</td>
-                <td><input type="text" name="gender" placeholder="<?=$gender?>"
+                <td><input type="text" name="gender" placeholder="<?= 'Введите пол питомца в виде М или Ж.' ?>"
                            pattern="[МмЖж]" required></td>
             </tr>
             <tr>
                 <td>Комментарии</td>
-                <td><input type="text" name="comments" placeholder="<?=$comments?>"></td>
+                <td><input type="text" name="comments" placeholder="<?= 'Комментарии.' ?>"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="add" value="Добавить" style="width: 100%" "></td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+<? } else { ?>
+    <form action="index.php" method="post">
+        <table border="1">
+            <thead>
+            <tr>
+                <td>Название поля</td>
+                <td>Поле ввода</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Id</td>
+                <td><input type="text" name="id" value="<?= $lastId ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>Имя животного</td>
+                <td><input type="text" name="name" value="<?= $name ?>"
+                           pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
+            </tr>
+            <tr>
+                <td>Вид животного</td>
+                <td><input type="text" name="species" value="<?= $species ?>"
+                           pattern="^[-А-ЯA-Zа-яa-zЁё\s]+$" required></td>
+            </tr>
+            <tr>
+                <td>Вес животного</td>
+                <td><input type="text" name="weight" value="<?= $weight ?>" pattern="^[0-9]+$"
+                           required></td>
+            </tr>
+            <tr>
+                <td>Пол животного</td>
+                <td><input type="text" name="gender" value="<?= $gender ?>"
+                           pattern="[МмЖж]" required></td>
+            </tr>
+            <tr>
+                <td>Комментарии</td>
+                <td><input type="text" name="comments" value="<?= $comments ?>"></td>
             </tr>
             <tr>
                 <td></td>
@@ -201,6 +203,6 @@ WHERE id = $editRow['.'id'.']');
             </tbody>
         </table>
     </form>
-<?}?>
+<? } ?>
 </body>
 </html>
