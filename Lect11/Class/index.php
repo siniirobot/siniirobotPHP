79ace937efaf36b88733262d56e5b5e022c757b6
Горбунов Петр <40553017+siniirobot@users.noreply.github.com>
2019-01-08 20:07:33 +0700

    Lect11 переделываем под проверку для докторов

--- Lect11/Class/index.php
+++ Lect11/Class/index.php
@@ -6,13 +6,13 @@
  * Time: 11:20
  */
 
-include_once __DIR__ . '/animalType.php';
+include_once __DIR__ . '/doctors.php';
 
 
-$cat = new animalType();
+$cat = new doctors();
 $cat = $cat->find(1);
-$cat->nameLAT = 24e214;
+$cat->nameLAT = 'Арнольд Шварцнегер';
 $cat->update();
-$olen = new animalType();
-$olen = $olen->find(18);
-$olen->delete();
+$olen = new doctors();
+$olen = $olen->find(2);
+var_dump($olen);
