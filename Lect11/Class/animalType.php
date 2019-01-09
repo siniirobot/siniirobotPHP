06caeca6c1ef529d3e2d38eabc756fb7083d7a1e
siniirobotRab <siniirobot@gmail.com>
2019-01-09 08:58:12 +0700

    Lect11 наработки с работы

--- Lect11/Class/animalType.php
+++ Lect11/Class/animalType.php
@@ -8,21 +8,42 @@
 
 class animalType
 {
-    public $query;
 
-    public function __construct()
-    {
-        require_once __DIR__ . '/DB.php';
-        $this->query = DB::pdo()->query('SELECT * FROM animalType');
+    public $nameRUS;
+    public $nameLAT;
+
+    public  function find($id) {
+        require_once __DIR__.'/DB.php';
+        $result  = DB::pdo()->prepare('SELECT * FROM animalType WHERE id = ?');
+        $result->execute([$id]);
+        $result = $result->fetch(PDO::FETCH_ASSOC);
+        if ($result){
+            $this->nameRUS = $result['nameRUS'];
+            $this->nameLAT = $result['nameLAT'];
+            return 'Ваши данные найдены';
+        }else{
+            return 'Нет такого id';
+        }
     }
 
-    public function getFind($id) {
-
-        while ($row = $this->query->fetch()){
-            if ($id == $row['id']){
-                return $row['nameRUS'].' '.$row['nameLAT'];
-            }
+    public function read(){
+        require_once __DIR__.'/DB.php';
+        $result = DB::pdo()->prepare('SELECT * FROM animalType');
+        $result->execute();
+        $result = $result->fetchAll();
+        var_dump($result);
+        $this->nameRUS = $result['nameRUS'];
+        $this->nameLAT = $result['nameLAT'];
+        var_dump($this->nameRUS);
+        var_dump($this->nameLAT);
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+            echo $row['id'].'|'.$row['nameRUS'].'|'.$row['nameLAT'].'</br>';
         }
-        return'Нету такого id';
+    }
+
+    public function save() {
+        require_once __DIR__.'/DB.php';
+        $result  = DB::pdo()->prepare('INSERT INTO animalType (nameRUS,nameLAT) VALUE (?,?)');
+        $result->execute([$this->nameRUS,$this->nameLAT]);
     }
 }
--- Lect11/Class/index.php
+++ Lect11/Class/index.php
@@ -10,5 +10,10 @@ include_once __DIR__ . '/animalType.php';
 
 $animalType = new animalType();
 
-echo $animalType->getFind(16);
-
+$animalType->find(6);
+echo $animalType->nameRUS.'</br>';
+echo $animalType->nameLAT.'</br>';
+$animalType->nameRUS = 'Синица';
+$animalType->nameLAT ='Sinica';
+//$animalType->save();
+echo $animalType->read();
