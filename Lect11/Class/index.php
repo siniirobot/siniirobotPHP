aa7210fe8c3ad31bc63bc26f2f415935e3eeb64e
siniirobotRab <siniirobot@gmail.com>
2019-01-09 14:30:54 +0700

    Lect 11 ljhf,jnrf

--- Lect11/Class/index.php
+++ Lect11/Class/index.php
@@ -7,21 +7,20 @@
  */
 
 include_once __DIR__ . '/doctors.php';
-$tel = '8-913-907-74-08';
-//var_dump(preg_match('/^((\+?7|8)[\s \-]?){1}((\(\d{3}\))|(\d{3})){1}([\s \-]?){1}(\d{3}[\s \-]?\d{2}[\s \-]?\d{2}){1}$/',$tel));
+
 $pokemon = new doctors();
 $pokemon->lastName = 'Pikachu';
 $pokemon->name = 'Pika';
-$pokemon->phone = '+7-(951)-368-86-64';
+$pokemon->phone = '+7-(155)-754-24-65';
 $pokemon->salary = 35600.50;
 $pokemon->receipt_date = '2019-01-17';
-//$pokemon->create();
+$pokemon->create();
 
 $lastRow = new doctors();
 $lastRow->read();
 echo $lastRow->lastName .'</br>'. $lastRow->name.'</br>'.$lastRow->phone.'</br>'.$lastRow->salary.'</br>'.$lastRow->receipt_date.'</br>';
 
-echo 'Поиск</br>';
+
 $findRow = new doctors();
 $findRow = $findRow->find(1);
 echo $findRow->lastName .'</br>'. $findRow->name.'</br>'.$findRow->phone.'</br>'.$findRow->salary.'</br>'.$findRow->receipt_date.'</br>';
@@ -33,5 +32,8 @@ $findRow->receipt_date = '2500-12-18';
 $findRow->update();
 
 $deleteRow = new doctors();
-$deleteRow = $deleteRow->find(30);
+$deleteRow = $deleteRow->find(47);
 $deleteRow->delete();
+
+$some = new doctors();
+$some->delete();
