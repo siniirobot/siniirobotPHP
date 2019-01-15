5391a273135ecb1cebd3c16e7d0514568c6ddf21
siniirobotRab <siniirobot@gmail.com>
2019-01-15 15:47:43 +0700

    final CSS

---
+++ final/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HEAD
+ * Date: 15.01.2019
+ * Time: 10:55
+ */
+
+?>
+
+
+<head>
+    <title>Ветеренарная клиника</title>
+    <link href="css/CSSReset.css" rel="stylesheet">
+    <link href="css/CSS.css" rel="stylesheet">
+</head>
+<body>
+    <div class="login_form">
+        <form action="index.php" method="post">
+            <input type="text" name="login" placeholder="Введите свой логин.">
+            </br>
+            <input type="password" name="password" placeholder="Введите свой пароль.">
+            </br>
+            <input type="submit" name="Ok" value="Войти">
+            </br>
+            <a>Не могу войти</a>
+            </br>
+            <a>Регистрация</a>
+            </br>
+        </form>
+    </div>
+</body>
